b7dcecaef5cd20fae584b99402090cf85ffb7d82
agmarchuk <mag@iis.nsk.su>
2021-06-06 22:52:39 +0700

    Добавил документ с курсом, начал делать RDF-движок

---
+++ RDFEngine/IEngine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+
+namespace RDFEngine
+{
+    public interface IEngine
+    {
+        public void Clear();
+        public void Load(IEnumerable<XElement> records);
+        public void Build();
+        public IEnumerable<XElement> Search(string searchstring);
+        public XElement GetRecord(string id, bool addinverse);
+    }
+}
