b1aaec3871bf2733af2ce48c9d4ca404854aa489
Konstantin_Shishin <shishinkostya2000@gmail.com>
2021-07-18 12:39:14 +0300

    Konstantin2

---
+++ Konstantin2/Infobase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RDFEngine;
+
+namespace Konstantin2
+{
+    public class Infobase
+    {
+        public static RDFEngine.REngine engine;
+    }
+}
