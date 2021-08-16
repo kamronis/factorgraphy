2b757ddf45172f69de48784b53fb6409a54715bd
agmarchuk <mag@iis.nsk.su>
2021-08-16 13:59:54 +0700

    Продолжение формирования учебного курса

--- FactographyView/Controllers/HomeController.cs
+++ FactographyView/Controllers/HomeController.cs
@@ -33,13 +33,58 @@ namespace FactographyView.Controllers
             var model = Infobase.engine.GetRRecord(id);
             return View("Portrait", model);
         }
-        public IActionResult ShowTree(string id)
+        //public IActionResult ShowTree(string id)
+        //{
+        //    var model = Infobase.engine.GetRTree(id, 2, null);
+        //    return View("ShowTree", model);
+        //}
+
+        private static string BuildPortraitText(string id, int level, string forbidden)
         {
-            var model = Infobase.engine.GetRTree(id, 2, null);
-            return View("ShowTree", model);
+            var rec = Infobase.engine.GetRRecord(id);
+            if (rec == null) return null;
+            string result = "{Id:" + rec.Id + ", Tp:" + rec.Tp + ", " +
+                rec.Props.Select<RDFEngine.RProperty, string>(p =>
+                {
+                    if (p is RDFEngine.RField) return p.Prop + ":" + ((RDFEngine.RField)p).Value;
+                    else if (level > 0 && p is RDFEngine.RLink && p.Prop != forbidden) return p.Prop + ":" + BuildPortraitText(((RDFEngine.RLink)p).Resource, 0, null);
+                    else if (level > 1 && p is RDFEngine.RInverseLink) return p.Prop + ":" + BuildPortraitText(((RDFEngine.RInverseLink)p).Source, 1, p.Prop);
+                    return null;
+                }).Where(s => s != null).Aggregate((a, s) => a + ", " + s) + "}";
+            return result;
+        }
+        public IActionResult Portrait1(string id)
+        {
+            var model = BuildPortraitText(id, 2, null);
+            return View("Portrait1", model);
         }
 
-
+        private static RDFEngine.RRecord BuildPortrait(string id, int level, string forbidden)
+        {
+            var rec = Infobase.engine.GetRRecord(id);
+            if (rec == null) return null;
+            RDFEngine.RRecord result_rec = new RDFEngine.RRecord()
+            {
+                Id = rec.Id,
+                Tp = rec.Tp,
+                Props = rec.Props.Select<RDFEngine.RProperty, RDFEngine.RProperty>(p => 
+                {
+                    if (p is RDFEngine.RField)
+                        return new RDFEngine.RField() { Prop = p.Prop, Value = ((RDFEngine.RField)p).Value };
+                    else if (level > 0 && p is RDFEngine.RLink && p.Prop != forbidden)
+                        return new RDFEngine.RDirect() { Prop = p.Prop, DRec = BuildPortrait(((RDFEngine.RLink)p).Resource, 0, null) };
+                    else if (level > 1 && p is RDFEngine.RInverseLink)
+                        return new RDFEngine.RInverse() { Prop = p.Prop, IRec = BuildPortrait(((RDFEngine.RInverseLink)p).Source, 1, p.Prop) };
+                    return null;
+                }).Where(p => p != null).ToArray()
+            };
+            return result_rec;
+        }
+        public IActionResult Portrait2(string id)
+        {
+            var model = BuildPortrait(id, 2, null);
+            return View("Portrait2", model);
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
@@ -48,3 +93,4 @@ namespace FactographyView.Controllers
         }
     }
 }
+
